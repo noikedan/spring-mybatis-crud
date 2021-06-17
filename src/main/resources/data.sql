8e012dc019c80ce20e59ae47f04a05d7b05e67bb
Gina <you@example.com>
2021-06-18 08:02:32 +0900

    SQLをPostgreSQL用に変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO staff(name, email, status, registration)
-VALUES('中池', 'nakaike@example.com', 't', sysdate);
+VALUES('中池', 'nakaike@example.com', 't', CURRENT_DATE);
 INSERT INTO staff(name, email, status, registration)
-VALUES('田中', 'tanaka@example.com', 't', sysdate);
+VALUES('田中', 'tanaka@example.com', 't', CURRENT_DATE);
